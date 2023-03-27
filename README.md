404ec17806434591d0178a158078fe635eebbccc
Dennis Baldwin <dennisbaldwin@gmail.com>
2023-03-27 17:38:02 -0500

    Hooking this up to Go1!

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-Tested with Node v18.15.0
+# Notes
 
-Requires two network adapters so you can connect to Go1 and the internet at the same time.
+Tested with Node v18.12.1
 
-Requires an OpenAPI API key.
+Requires an OpenAPI API key
+
+Requires two network adapters so you can connect to Go1 and the ChatGPT API at the same time.
+
+# Get up and running
+
+- npm i
+- cp .env.example .env
+- Update OPENAPI_API_KEY with your key
+- Power up Go1
+- Connect to Go1's wifi from your second adapter
+- node index.js
+- Ask it some cool questions!
--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 import { createInterface } from "node:readline/promises";
 import { stdin as input, stdout as output, env } from "node:process";
 import { Configuration, OpenAIApi } from "openai";
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv"
+dotenv.config();
+
+import { Go1, Go1Mode } from "@droneblocks/go1-js"
 
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 const readline = createInterface({ input, output });
-
-class Go1 {
-  constructor() {
-    console.log('dog created')
-  }
-
-  moveForward = (speed, duration) => {
-    console.log(`moving forward at ${speed} for ${duration}`)
-  }
-}
 
 let dog = new Go1();
 
@@ -28,23 +20,38 @@ const extractJSCode = (content) => {
   return codeBlock
 }
 
-// const chatbotType = await readline.question(
-//   "What type of chatbot would you like to create? "
-// );
+const chatPrompt = `You are an assistant that is helping me control the Unitree Go1 quadruped robot dog. All programming of the robot will be done with the @droneblocks/go-1js node library.
 
-const chatbotType = `You are an assistant that is helping me control the Unitree Go1 quadruped robot. All programming of the robot will be done with the @droneblocks/go-1js node library.
+I've already taken care of importing the library and created an instance of the class called dog. The method definitions for moving Go1 are defined here:
 
-I've already taken care of importing the library and created an instance of the class called dog. The methods you can use are
-
-dog.moveForward
-dog.moveBackward
-dog.moveLeft
-dog.moveRight
+goForward(speed: number, lengthOfTime: number)
+goBackward(speed: number, lengthOfTime: number)
+goLeft(speed: number, lengthOfTime: number)
+goRight(speed: number, lengthOfTime: number)
+turnLeft(speed: number, lengthOfTime: number)
+turnRight(speed: number, lengthOfTime: number)
 
 All methods accept two arguments the first is speed with a value from 0 to 1 where 1 is full speed. The second argument is a duration in milliseconds.
+
+Also, please keep in mind that before we can move Go1 we need to make sure to set it's mode to Go1Mode.walk.
+
+There is a wait method in cases where we want to pause between commands. It accepts a number in milliseconds:
+
+wait(lengthOfTime: number)
+
+There are also stances such as beg, which the method definition looks like this:
+
+setMode(Go1Mode.straightHand1)
+
+You can also change the LEDs of the robot dog using the following:
+
+setLedColor(red: number, green: number, blue: number)
+
+Where red, green, and blue are integers between 0 and 255.
+
 `
 
-const messages = [{ role: "system", content: chatbotType }];
+const messages = [{ role: "system", content: chatPrompt }];
 
 let userInput = await readline.question("Welcome to the Go1 chatbot. Please feel free to ask it for code to control your robot!\n>");
 
@@ -57,8 +64,6 @@ while (userInput !== ".quit") {
     });
 
     const botMessage = response.data.choices[0].message;
-    
-    //extractJSCode(botMessage.content)
 
     if (botMessage) {
       messages.push(botMessage);
@@ -71,7 +76,9 @@ while (userInput !== ".quit") {
       let confirmCode = await readline.question("Does the code look good to run? (y/n)")
 
       if (confirmCode == "y") {
-        console.log("executing code")
+
+        let program = await eval(extractJSCode(botMessage.content))
+
       } else {
         console.log("Sorry. Please specify more details.\n")
       }
